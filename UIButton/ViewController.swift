9f9d2d1518ed579517bf8af397339713f336e355
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-01 00:55:49 +0900

    UIButton

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var okButton: UIButton!
+    
+    
+    @IBAction func random(_ sender: Any) {
+        let num = arc4random_uniform(100)
+        numLabel.text = String(num)
+        
+        okButton.isEnabled = (num>=50)
+    }
+    
+    
+    @IBAction func tapOK(_ sender: Any) {
+        print("OK")
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
